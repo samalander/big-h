3aa38ee6d315ecfd24ac5670378a0dff23d4e508
Eric Greve <ericgreve@gmail.com>
2013-07-07 16:51:47 -0400

    Added showing spelled-out English, French and Spanish weekdays as a setting.

--- src/big-h.c
+++ src/big-h.c
@@ -18,6 +18,13 @@ PBL_APP_INFO(MY_UUID,
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_WATCH_FACE);
 
+#define WEEKDAY_FORMAT_INTL 0
+// [[EN]]
+#define WEEKDAY_FORMAT_EN 1
+// [[FR]]
+#define WEEKDAY_FORMAT_FR 2
+// [[ES]]
+#define WEEKDAY_FORMAT_ES 3
 #define DATE_FORMAT_AUTO "auto"
 
 /************** SETTINGS - START **************/
@@ -33,6 +40,9 @@ PBL_APP_INFO(MY_UUID,
 // [[M1]]
 // set to true to make Monday the first day of the week in the international weekday display (no effect on other weekday displays)
 #define WEEKDAY_MONDAY_FIRST false
+
+// see variables above to select a language for the weekday display
+#define WEEKDAY_FORMAT WEEKDAY_FORMAT_INTL
 
 /*
  * By default, the date will be YYYY-MM-DD if the clock is 24-hour style and MM-DD-YYYY if the clock is 12-hour style.
@@ -55,7 +65,12 @@ PBL_APP_INFO(MY_UUID,
 
 Window window;
 Layer weekday_background_layer, weekday_text_layer, date_background_layer, date_text_layer, hours_layer, minutes_layer, seconds_background_layer, seconds_indicator_layer, ampm_layer;
-BmpContainer date_digits[10], time_digits[10], weekday_digits[2][10];
+BmpContainer date_digits[10], time_digits[10];
+#if WEEKDAY_FORMAT == WEEKDAY_FORMAT_INTL
+  BmpContainer weekday_digits[2][10];
+#else
+  BmpContainer weekday_name[7];
+#endif
 static int mday_max[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 // Global time to have access to it in draw events
@@ -67,107 +82,113 @@ void paint_weekday_background(Layer *layer, GContext *ctx) {
   // draw the vertical line on the right
   graphics_draw_line(ctx, GPoint(14, 0), GPoint(14, 167));
 
-  //draw the horizontal lines that separate the 7 weekday slots
-  graphics_draw_line(ctx, GPoint(0, 24), GPoint(13, 24));
-  graphics_draw_line(ctx, GPoint(0, 48), GPoint(13, 48));
-  graphics_draw_line(ctx, GPoint(0, 72), GPoint(13, 72));
-  graphics_draw_line(ctx, GPoint(0, 96), GPoint(13, 96));
-  graphics_draw_line(ctx, GPoint(0, 120), GPoint(13, 120));
-  graphics_draw_line(ctx, GPoint(0, 144), GPoint(13, 144));
-}
-
-
-// draw the content of one slot in the weekday column
-void paint_weekday_text_single(GContext *ctx, int position) {
-  // day of the month being drawn, based on the current day
-  int mday = tick_time.tm_mday;
-  // day of the week being drawn, base on the current weekday
-  int wday = tick_time.tm_wday;
-  #if WEEKDAY_MONDAY_FIRST
-    wday = (wday + 6) % 7;
+  #if WEEKDAY_FORMAT == WEEKDAY_FORMAT_INTL
+    //draw the horizontal lines that separate the 7 weekday slots
+    graphics_draw_line(ctx, GPoint(0, 24), GPoint(13, 24));
+    graphics_draw_line(ctx, GPoint(0, 48), GPoint(13, 48));
+    graphics_draw_line(ctx, GPoint(0, 72), GPoint(13, 72));
+    graphics_draw_line(ctx, GPoint(0, 96), GPoint(13, 96));
+    graphics_draw_line(ctx, GPoint(0, 120), GPoint(13, 120));
+    graphics_draw_line(ctx, GPoint(0, 144), GPoint(13, 144));
   #endif
-  // difference between the position we're drawing and the current weekday
-  int mday_delta = position - wday;
-
-  if (mday_delta < -3) {
-    // if the day delta is too low, we actually draw a day in the future instead (Sunday to Tuesday when currently Thursday or later in the week)
-    mday_delta += 7;
-  }
-  else if (mday_delta > 3) {
-    // if the day delta is too high, we actually draw a day in the past instead (Thursday to Saturday when currently Tuesday or earlier in the week)
-    mday_delta -= 7;
-  }
-
-
-  // to separate the weekdays in the past from those in the future, we draw an arrow on the border (except if that border is spread out on the screen edges)
-  int line;
-  switch (mday_delta) {
-    // draw the bottom part of the arrow
-    case -3:
-      if (position > 0) {
-        line = position * 24;
-        for (int i = 2; i < 7; i += 2) {
-          graphics_draw_line(ctx, GPoint(i, ++line), GPoint(13 - i, line));
-        }
-      }
-      break;
-    // draw the top part of the arrow
-    case 3:
-      if (position < 6) {
-        line = (position + 1) * 24;
-        for (int i = 0; i < 6; i += 2) {
-          graphics_draw_line(ctx, GPoint(0, --line), GPoint(5 - i, line));
-          graphics_draw_line(ctx, GPoint(8 + i, line), GPoint(13, line));
-        }
-      }
-      break;
-  }
-
-  // decide what the day of the month we're drawing actually is
-  mday += mday_delta;
-  if (mday < 1) {
-    // if we're in the past and accross a month's start, get the last day of the previous month and count from there
-    int new_month = tick_time.tm_mon - 1;
-    // watch out for December...
-    if (new_month == -1) {
-      new_month = 11;
-    }
-    mday += mday_max[new_month];
-    // ... and February on leap years (not checking for the 100s and 400s rules since they won't occur for 87 years...)
-    if ((new_month == 1) && ((tick_time.tm_year + 1900) % 4 == 0)) {
-      mday++;
-    }
-  }
-  else if (mday > mday_max[tick_time.tm_mon]) {
-    // if we're in the future and past the current month's end, start from 1 in the next month
-    int mday_mod = mday_max[tick_time.tm_mon];
-    // watch out for February in leap years here too
-    if ((tick_time.tm_mon == 1) && ((tick_time.tm_year + 1900) % 4 == 0)) {
-      mday_mod++;
-    }
-    mday %= mday_mod;
-  }
-  // decide where to draw and in what color
-  int base_y = (position * 24) + 12 - 5;
-  int color = (bool)(wday == position);
-  if (color) {
-    // if we're drawing today's date, we need to fill in the background in white
-    graphics_context_set_fill_color(ctx, GColorWhite);
-    graphics_fill_rect(ctx, GRect(0, position * 24, 14, 24), 0, GCornerNone);
-  }
-  // draw the actual digits from what we decided above
-  if (mday >= 10) {
-    graphics_draw_bitmap_in_rect(ctx, &weekday_digits[color][mday / 10].bmp, GRect(0, base_y, 6, 11));
-  }
-  graphics_draw_bitmap_in_rect(ctx, &weekday_digits[color][mday % 10].bmp, GRect(7, base_y, 6, 11));
 }
 
+#if WEEKDAY_FORMAT == WEEKDAY_FORMAT_INTL
+  // draw the content of one slot in the weekday column
+  void paint_weekday_text_single(GContext *ctx, int position) {
+    // day of the month being drawn, based on the current day
+    int mday = tick_time.tm_mday;
+    // day of the week being drawn, base on the current weekday
+    int wday = tick_time.tm_wday;
+    #if WEEKDAY_MONDAY_FIRST
+      wday = (wday + 6) % 7;
+    #endif
+    // difference between the position we're drawing and the current weekday
+    int mday_delta = position - wday;
+
+    if (mday_delta < -3) {
+      // if the day delta is too low, we actually draw a day in the future instead (Sunday to Tuesday when currently Thursday or later in the week)
+      mday_delta += 7;
+    }
+    else if (mday_delta > 3) {
+      // if the day delta is too high, we actually draw a day in the past instead (Thursday to Saturday when currently Tuesday or earlier in the week)
+      mday_delta -= 7;
+    }
+
+
+    // to separate the weekdays in the past from those in the future, we draw an arrow on the border (except if that border is spread out on the screen edges)
+    int line;
+    switch (mday_delta) {
+      // draw the bottom part of the arrow
+      case -3:
+        if (position > 0) {
+          line = position * 24;
+          for (int i = 2; i < 7; i += 2) {
+            graphics_draw_line(ctx, GPoint(i, ++line), GPoint(13 - i, line));
+          }
+        }
+        break;
+      // draw the top part of the arrow
+      case 3:
+        if (position < 6) {
+          line = (position + 1) * 24;
+          for (int i = 0; i < 6; i += 2) {
+            graphics_draw_line(ctx, GPoint(0, --line), GPoint(5 - i, line));
+            graphics_draw_line(ctx, GPoint(8 + i, line), GPoint(13, line));
+          }
+        }
+        break;
+    }
+
+    // decide what the day of the month we're drawing actually is
+    mday += mday_delta;
+    if (mday < 1) {
+      // if we're in the past and accross a month's start, get the last day of the previous month and count from there
+      int new_month = tick_time.tm_mon - 1;
+      // watch out for December...
+      if (new_month == -1) {
+        new_month = 11;
+      }
+      mday += mday_max[new_month];
+      // ... and February on leap years (not checking for the 100s and 400s rules since they won't occur for 87 years...)
+      if ((new_month == 1) && ((tick_time.tm_year + 1900) % 4 == 0)) {
+        mday++;
+      }
+    }
+    else if (mday > mday_max[tick_time.tm_mon]) {
+      // if we're in the future and past the current month's end, start from 1 in the next month
+      int mday_mod = mday_max[tick_time.tm_mon];
+      // watch out for February in leap years here too
+      if ((tick_time.tm_mon == 1) && ((tick_time.tm_year + 1900) % 4 == 0)) {
+        mday_mod++;
+      }
+      mday %= mday_mod;
+    }
+    // decide where to draw and in what color
+    int base_y = (position * 24) + 12 - 5;
+    int color = (bool)(wday == position);
+    if (color) {
+      // if we're drawing today's date, we need to fill in the background in white
+      graphics_context_set_fill_color(ctx, GColorWhite);
+      graphics_fill_rect(ctx, GRect(0, position * 24, 14, 24), 0, GCornerNone);
+    }
+    // draw the actual digits from what we decided above
+    if (mday >= 10) {
+      graphics_draw_bitmap_in_rect(ctx, &weekday_digits[color][mday / 10].bmp, GRect(0, base_y, 6, 11));
+    }
+    graphics_draw_bitmap_in_rect(ctx, &weekday_digits[color][mday % 10].bmp, GRect(7, base_y, 6, 11));
+  }
+#endif
 
 // draw the weekdays in all 7 slots
 void paint_weekday_text(Layer *layer, GContext *ctx) {
-  for (int i = 0; i < 7; i++) {
-    paint_weekday_text_single(ctx, i);
-  }
+  #if WEEKDAY_FORMAT == WEEKDAY_FORMAT_INTL
+    for (int i = 0; i < 7; i++) {
+      paint_weekday_text_single(ctx, i);
+    }
+  #else
+    graphics_draw_bitmap_in_rect(ctx, &weekday_name[tick_time.tm_wday].bmp, GRect(0, 0, 14, 168));
+  #endif
 }
 
 
@@ -438,26 +459,52 @@ void handle_init(AppContextRef ctx) {
   bmp_init_container(RESOURCE_ID_TIME_8, &time_digits[8]);
   bmp_init_container(RESOURCE_ID_DATE_9, &date_digits[9]);
   bmp_init_container(RESOURCE_ID_TIME_9, &time_digits[9]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_0, &weekday_digits[0][0]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_1, &weekday_digits[0][1]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_2, &weekday_digits[0][2]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_3, &weekday_digits[0][3]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_4, &weekday_digits[0][4]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_5, &weekday_digits[0][5]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_6, &weekday_digits[0][6]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_7, &weekday_digits[0][7]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_8, &weekday_digits[0][8]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_0_9, &weekday_digits[0][9]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_0, &weekday_digits[1][0]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_1, &weekday_digits[1][1]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_2, &weekday_digits[1][2]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_3, &weekday_digits[1][3]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_4, &weekday_digits[1][4]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_5, &weekday_digits[1][5]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_6, &weekday_digits[1][6]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_7, &weekday_digits[1][7]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_8, &weekday_digits[1][8]);
-  bmp_init_container(RESOURCE_ID_WEEKDAY_1_9, &weekday_digits[1][9]);
+  #if WEEKDAY_FORMAT == WEEKDAY_FORMAT_INTL
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_0, &weekday_digits[0][0]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_1, &weekday_digits[0][1]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_2, &weekday_digits[0][2]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_3, &weekday_digits[0][3]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_4, &weekday_digits[0][4]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_5, &weekday_digits[0][5]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_6, &weekday_digits[0][6]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_7, &weekday_digits[0][7]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_8, &weekday_digits[0][8]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_0_9, &weekday_digits[0][9]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_0, &weekday_digits[1][0]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_1, &weekday_digits[1][1]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_2, &weekday_digits[1][2]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_3, &weekday_digits[1][3]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_4, &weekday_digits[1][4]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_5, &weekday_digits[1][5]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_6, &weekday_digits[1][6]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_7, &weekday_digits[1][7]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_8, &weekday_digits[1][8]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_1_9, &weekday_digits[1][9]);
+  #elif WEEKDAY_FORMAT == WEEKDAY_FORMAT_EN
+    bmp_init_container(RESOURCE_ID_WEEKDAY_EN_0, &weekday_name[0]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_EN_1, &weekday_name[1]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_EN_2, &weekday_name[2]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_EN_3, &weekday_name[3]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_EN_4, &weekday_name[4]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_EN_5, &weekday_name[5]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_EN_6, &weekday_name[6]);
+  #elif WEEKDAY_FORMAT == WEEKDAY_FORMAT_FR
+    bmp_init_container(RESOURCE_ID_WEEKDAY_FR_0, &weekday_name[0]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_FR_1, &weekday_name[1]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_FR_2, &weekday_name[2]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_FR_3, &weekday_name[3]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_FR_4, &weekday_name[4]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_FR_5, &weekday_name[5]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_FR_6, &weekday_name[6]);
+  #elif WEEKDAY_FORMAT == WEEKDAY_FORMAT_ES
+    bmp_init_container(RESOURCE_ID_WEEKDAY_ES_0, &weekday_name[0]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_ES_1, &weekday_name[1]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_ES_2, &weekday_name[2]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_ES_3, &weekday_name[3]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_ES_4, &weekday_name[4]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_ES_5, &weekday_name[5]);
+    bmp_init_container(RESOURCE_ID_WEEKDAY_ES_6, &weekday_name[6]);
+  #endif
 
   // populate our time variable for our initial status
   get_time(&tick_time);
@@ -485,14 +532,21 @@ void handle_init(AppContextRef ctx) {
 void handle_deinit(AppContextRef ctx) {
   (void)ctx;
 
-  // unload all our digit images
+  // unload all our images
   for (int i = 0; i < 10; i++) {
     bmp_deinit_container(&date_digits[i]);
     bmp_deinit_container(&time_digits[i]);
-    for (int j = 0; j < 2; j++) {
-      bmp_deinit_container(&weekday_digits[j][i]);
-    }
+    #if WEEKDAY_FORMAT == WEEKDAY_FORMAT_INTL
+      for (int j = 0; j < 2; j++) {
+        bmp_deinit_container(&weekday_digits[j][i]);
+      }
+    #endif
   }
+  #if WEEKDAY_FORMAT == WEEKDAY_FORMAT_EN || WEEKDAY_FORMAT == WEEKDAY_FORMAT_FR || WEEKDAY_FORMAT == WEEKDAY_FORMAT_ES
+    for (int i = 0; i < 7; i++) {
+      bmp_deinit_container(&weekday_name[i]);
+    }
+  #endif
 }
 
 
